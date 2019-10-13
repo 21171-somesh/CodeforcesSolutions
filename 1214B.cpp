166ff392fcc79201b0cefe2a97ffb1728eb1fa3c
21171-somesh <33005692+21171-somesh@users.noreply.github.com>
2019-10-13 22:16:20 +0530

    Update 1214B.cpp

--- 1214B.cpp
+++ 1214B.cpp
@@ -2,19 +2,16 @@
 
 using namespace std;
 
-int main()
-{
-    long long b,g,n,bmax,gmax,gmin,bmin;
+int main() {
+    long long b, g, n, bmax, gmax, gmin, bmin;
+    cin >> b >> g >> n;
     
-    cin>>b>>g>>n;
+    bmax = min(n, b);
+    gmax = min(n, g);
     
-    bmax=min(n,b);
-    gmax=min(n,g);
+    gmin = min(n - bmax, gmax);
+    bmin = min(n - gmax, bmax);
     
-    gmin=min(n-bmax , gmax);
-    bmin=min(n-gmax , bmax);
-    
-    cout<<min(bmax-bmin+1,gmax-gmin+1);
-    
+    cout << min(bmax - bmin + 1, gmax - gmin + 1);
     return 0;
 }
